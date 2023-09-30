d119e4d4e9be0464177c22b8326e3be584c3a9e9
aldinalj <aldinalj@gmail.com>
2023-09-30 12:46:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(2 / 0);
+        } catch (Exception error) {
+            System.out.println("Wtf is a kilometer?");
+        }
+
+
+        try {
+            for (int i = 0; i < 2 / 0; i++) {
+                System.out.println("Lol");
+            }
+        } catch (Exception error) {
+            System.out.println("What would you do if when you okay so he said yes would go?");
+        }
+
+        int[] nambörs = {1, 5, 6, 9, 0, 3};
+
+        try {
+            if (true) {
+                System.out.println(nambörs[6]);
+            }
+        } catch (Exception error) {
+            System.out.println("Do you wear wigs?");
+        }
+
+    }
+}
